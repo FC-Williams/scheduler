9156264ed1f1955516b7f80c53adb8f80e53bc5d
Rafael Cerino Frias <rafacerino96@gmail.com>
2023-06-12 16:28:01 -0600

    Creacion de Clases

---
+++ src/main/java/com/scheduler/model/Activities.java
@@ -0,0 +1,156 @@
+package com.scheduler.model;
+
+import java.util.*;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "ACTIVITIES", schema = "SCHEDULE")
+public class Activities {
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long activityId;
+
+    private Long activityTypeId;
+
+    private Long statusId;
+
+    private String activity;
+
+    private String description;
+
+    private Date startDate;
+
+    private Date endDate;
+
+    private String username;
+
+    private Long areaId;
+    
+    @Column(name = "CREATION_DATE", nullable = false, updatable = false)
+    @CreationTimestamp
+    private Date creationDate;
+
+    // @Column(name = "LAST_UPDATE")
+    private Date lastUpdate;
+
+    public Status(){}
+
+    public Status(Long activityId, Long activityTypeId, Long statusId, String activity, String description, Date startDate, Date endDate, Date creationDate, Date lastUpdate, String username, Long areaId) {
+        this.activityId = activityId;
+        this.activityTypeId = activityTypeId;
+        this.statusId = statusId;
+        this.activity = activity;
+        this.description = description;
+        this.startstartDate =startDate;
+        this.endDate = endDate;
+        this.creationDate = creationDate;
+        this.lastUpdate = lastUpdate;
+        this.username = username;
+        this.areaId = areaId;
+    }
+
+
+    public Long getActivityId() {
+        return this.activityId;
+    }
+
+    public void setActivityId(Long activityId) {
+        this.activityId = activityId;
+    }
+
+    public Long getStatusId() {
+        return this.statusId;
+    }
+
+    public void setStatusId(Long statusId) {
+        this.statusId = statusId;
+    }
+
+    public Long getActivityTypeId() {
+        return this.activityTypeId;
+    }
+
+    public void setActivityTypeId(Long activityTypeId) {
+        this.activityTypeId = activityTypeId;
+    }
+
+    public String getActivity() {
+        return this.activity;
+    }
+
+    public void setActivity(String activity) {
+        this.activity = activity;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getStartDate() {
+        return this.startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return this.endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public Date getCreationDate() {
+        return this.creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Date getLastUpdate() {
+        return this.lastUpdate;
+    }
+
+    public void setLastUpdate(Date lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    @PreUpdate
+    public void setLastUpdate() {
+        this.lastUpdate = new Date();
+    }
+
+    public String getUsername(){
+        return this.username;
+    }
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public Long getAreaId(){
+        return this.areaId;
+    }
+
+    public void setAreaId(Long areaId){
+        this.areaId = areaId;
+    }
+
+}
